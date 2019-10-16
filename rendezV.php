f8d2aea721e7eb005c1a27fc2bf93dcb9d8c233e
alamine25 <diamohamadoulamine@gmail.com>
2019-10-16 07:20:52 +0000

    rv.css ajouter

--- rendezV.php
+++ rendezV.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="styles2.css">
+    <link rel="stylesheet" href="rv.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
