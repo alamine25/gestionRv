ea204f07eedcbd71cdc9c212e5691748aecd945c
alamine25 <diamohamadoulamine@gmail.com>
2019-10-16 07:31:48 +0000

    image inserer

--- homepageM.php
+++ homepageM.php
@@ -20,7 +20,7 @@
     <!-- The sidebar -->
     <div class="sidebar">
         <span class="avatar">
-                <img src="img/images.jpeg" alt="" style="width:5rem ">
+                <img src="img/user8-512.png" alt="" style="width:5rem ">
                 <h4>Dr Ousmane</h4>
                 <h5>Medecin</h5>
         </span>
